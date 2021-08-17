ade74ef0097fdfe3ca5f9e8fff541968741f26e5
Martinfzz <forget.l.martin@gmail.com>
2021-08-17 14:41:50 +0200

    Remove profile controller + add params and render helpers

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :invitations, controller: 'rails_jwt_auth/invitations', only: [:show, :create, :update]
   resources :reset_passwords, controller: 'rails_jwt_auth/reset_passwords', only: [:show, :create, :update]
   resources :confirmations, controller: 'rails_jwt_auth/confirmations', only: [:create, :update]
-  resource :profile, controller: 'profile', only: %i[show update destroy] do
+  resource :profile, controller: 'rails_jwt_auth/profiles', only: %i[show update destroy] do
     collection do
       put :email
       put :password
